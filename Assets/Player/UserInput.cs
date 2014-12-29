c74095c5e50c312d581810d60a5c063c29a2d02a
Ashley Wilson <lem2j@hotmail.com>
2014-12-29 00:46:26 +0000

    Initial

---
+++ Assets/Player/UserInput.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInput : MonoBehaviour {
+
+    public float rotateAmount = 10f;
+    public float rotateSpeed = 100f;
+    public float scrollSpeed = 25f;
+
+    private Ship selectedShip;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCamera();
+        RotateCamera();
+        MouseActivity();
+	}
+
+    private void MoveCamera() {
+        float xpos = Input.mousePosition.x;
+        float ypos = Input.mousePosition.y;
+        bool mouseScroll = false;
+        Vector3 movement = new Vector3(0, 0, 0);
+
+        // Direction keys
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            movement.z += scrollSpeed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            movement.z -= scrollSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            movement.x += scrollSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            movement.x -= scrollSpeed;
+        }
+
+        // Make sure movement is in the direction the camera is pointing
+        // but ignore the vertical tilt of the camera to get sensible scrolling
+        movement = Camera.main.transform.TransformDirection(movement);
+        movement.y = 0;
+
+        // Away from ground movement
+        movement.y = scrollSpeed * Input.GetAxis("Mouse ScrollWheel");
+
+        // Calculate desired camera position based on received input
+        Vector3 origin = Camera.main.transform.position;
+        Vector3 destination = origin;
+        destination.x += movement.x;
+        destination.y += movement.y;
+        destination.z += movement.z;
+
+        // If a change in position is detected perform the necessary update
+        if (destination != origin) {
+            Camera.main.transform.position = Vector3.MoveTowards(origin, destination, Time.deltaTime * scrollSpeed);
+        }
+    }
+
+    private void RotateCamera() {
+        Vector3 origin = Camera.main.transform.eulerAngles;
+        Vector3 destination = origin;
+
+        // Detect rotation amount if ALT is being held and the Right mouse button is down
+        if (Input.GetMouseButton(1)) {
+            destination.x -= Input.GetAxis("Mouse Y") * rotateAmount;
+            destination.y += Input.GetAxis("Mouse X") * rotateAmount;
+        }
+
+        // If a change in position is detected perform the necessary update
+        if (destination != origin) {
+            Camera.main.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    private void MouseActivity() {
+        if (Input.GetMouseButtonDown(0)) {
+            LeftMouseClick();
+        } else if (Input.GetMouseButton(1)) {
+            RightMouseClick();
+        }
+
+        // MouseHover();
+    }
+
+    private void LeftMouseClick() {
+        GameObject hitObject = FindHitObject(Input.mousePosition);
+
+        if (hitObject) {
+            Ship ship = hitObject.transform.parent.GetComponent<Ship>();
+
+            if (ship) {
+                selectedShip = ship;
+                ship.SetSelection(true);
+            }
+        }
+    }
+
+    private void RightMouseClick() {
+        if (selectedShip) {
+            selectedShip.SetSelection(false);
+            selectedShip = null;
+        }
+    }
+
+    private GameObject FindHitObject(Vector3 origin) {
+        Ray ray = Camera.main.ScreenPointToRay(origin);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            return hit.collider.gameObject;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ Assets/Ships/Ship.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Ship : MonoBehaviour {
+
+    protected Rect playingArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+
+    private bool currentlySelected = false;
+    private Bounds selectionBounds;
+
+	// Use this for initialization
+	void Start () {
+        CalculateBounds();
+        playingArea = GetPlayingArea();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void SetSelection(bool selected) {
+        currentlySelected = selected;
+    }
+
+    protected virtual void OnGUI() {
+        if (currentlySelected) {
+            DrawSelection();
+        }
+    }
+
+    public Rect GetPlayingArea() {
+        return new Rect(0, 0, Screen.width, Screen.height);
+    }
+
+    public void CalculateBounds() {
+        selectionBounds = new Bounds(transform.position, Vector3.zero);
+        foreach (Renderer r in GetComponentsInChildren<Renderer>()) {
+            selectionBounds.Encapsulate(r.bounds);
+        }
+    }
+
+    private void DrawSelection() {
+        //GUI.skin = ResourceManager.SelectBoxSkin;
+        Rect selectBox = CalculateSelectionBox(selectionBounds, playingArea);
+        // Draw the selection box around the currently selection object, within the bounds of the playing area
+        GUI.BeginGroup(playingArea);
+        DrawSelectionBox(selectBox);
+        GUI.EndGroup();
+    }
+
+    protected virtual void DrawSelectionBox(Rect selectBox) {
+        GUI.Box(selectBox, "");
+    }
+
+    private Rect CalculateSelectionBox(Bounds selectionBounds, Rect playingArea) {
+        // Shorthand for the coordinates of the centre of the selection bounds
+        float cx = selectionBounds.center.x;
+        float cy = selectionBounds.center.y;
+        float cz = selectionBounds.center.z;
+
+        // Shorthand for the coordinates of the extends of the selection bounds
+        float ex = selectionBounds.extents.x;
+        float ey = selectionBounds.extents.y;
+        float ez = selectionBounds.extents.z;
+
+        // Determine the screen coordinates for the corners of the selection bounds
+        List<Vector3> corners = new List<Vector3>();
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx + ex, cy + ey, cz + ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx + ex, cy + ey, cz - ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx + ex, cy - ey, cz + ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx - ex, cy + ey, cz + ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx + ex, cy - ey, cz - ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx - ex, cy - ey, cz + ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx - ex, cy + ey, cz - ez)));
+        corners.Add(Camera.main.WorldToScreenPoint(new Vector3(cx - ex, cy - ey, cz - ez)));
+
+        // Determine the bounds on screen for the selection bounds
+        Bounds screenBounds = new Bounds(corners[0], Vector3.zero);
+        for (int i = 1; i < corners.Count; i++) {
+            screenBounds.Encapsulate(corners[i]);
+        }
+
+        // Screen coordinates start in the bottom left corner, rather than the top left corner
+        // this correction is needed to make sure the selection box is drawn in the correct place
+        float selectBoxTop = playingArea.height - (screenBounds.center.y + screenBounds.extents.y);
+        float selectBoxLeft = screenBounds.center.x - screenBounds.extents.x;
+        float selectBoxWidth = 2 * screenBounds.extents.x;
+        float selectBoxHeight = 2 * screenBounds.extents.y;
+
+        return new Rect(selectBoxLeft, selectBoxTop, selectBoxWidth, selectBoxHeight);
+    }
+}
